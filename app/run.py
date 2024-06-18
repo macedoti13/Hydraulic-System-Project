74aec918b6f79ecdbc6b8273c128752054914306
Thiago Macedo <thialmacedo@gmail.com>
2024-06-17 23:30:26 -0300

    FUNCTIONAL: more stuff

--- app/run.py
+++ app/run.py
@@ -8,14 +8,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # generate datasets paths 
 water_consumption_path = os.path.join(BASE_DIR, 'data', 'curated_datasets', 'water_consumption_curated.parquet')
-forecasting_dataset_paht = os.path.join(BASE_DIR, 'data', 'curated_datasets', 'forecasting_dataset.parquet')
+forecasting_dataset_path = os.path.join(BASE_DIR, 'data', 'curated_datasets', 'forecasting_dataset.parquet')
 
 # load datasets
 water_consumption = pd.read_parquet(water_consumption_path)
-forecasting_dataset = pd.read_parquet(forecasting_dataset_paht)
+forecasting_dataset = pd.read_parquet(forecasting_dataset_path)
 
 # import from the utils module here
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from utils.scripts.functions.main_functions import simulate_emptying, forecast_next_24_hours_output_flow_rate
 
 # generate app
 app = Flask(__name__)
@@ -31,8 +32,17 @@ def static_plots():
     return render_template('static_plots.html')
 
 # Route for the forecasting plots page
-@app.route('/forecasting-plots')
-def user_input():
+@app.route('/forecasting-plots', methods=['GET', 'POST'])
+def foresting_plots():
+    if request.method == 'POST':
+        year = request.form['year']
+        month = request.form['month']
+        day = request.form['day']
+        hour = request.form['hour']
+        minute = request.form['minute']
+            
+        return render_template('forecasting_plots.html')
+    
     return render_template('forecasting_plots.html')
 
 # Route for the model performance page
